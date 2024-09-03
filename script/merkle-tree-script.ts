6d4d0c2071466eb93a93edeb62ebcc856eb46508
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-03 12:12:02 +0100

    created merkle tree script

---
+++ script/merkle-tree-script.ts
@@ -0,0 +1,44 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import { parse } from "csv-parse";
+import { parseUnits } from "ethers";
+import { createReadStream } from "node:fs";
+import { resolve } from "node:path";
+
+const filePath: string = resolve("data", "records.csv");
+type RecordType = { address: string; amount: string };
+
+export async function generateMerkleTree(): Promise<
+  StandardMerkleTree<(string | bigint)[]>
+> {
+  const parser = parse({ columns: true }) as unknown as NodeJS.ReadWriteStream;
+  const records: RecordType[] = [];
+
+  return new Promise((resolve, reject) => {
+    parser.on("readable", () => {
+      let record: string | Buffer;
+      while ((record = parser.read())) {
+        records.push(record as unknown as RecordType);
+      }
+    });
+
+    parser.on("error", (err) => {
+      console.error(`Error parsing file ${filePath}`);
+      reject(err);
+    });
+
+    parser.on("end", () => {
+      console.info(`Finished parsing file ${filePath}`);
+
+      const parsedRecords = records.map((i) => [
+        i.address,
+        parseUnits(i.amount, 18),
+      ]);
+
+      const tree = StandardMerkleTree.of(parsedRecords, ["address", "uint256"]);
+      resolve(tree);
+    });
+
+    const fileStream = createReadStream(filePath);
+    fileStream.pipe(parser);
+  });
+}
